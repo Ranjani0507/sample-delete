218edc99a959f91a9bfd4b45cfa41375c3184f6f
ranjani <ranjaniindhra2006@gmail.com>
2026-01-06 23:04:46 +0530

    first Java GitHub practice

---
+++ hi.java
@@ -0,0 +1,8 @@
+public class hi {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub!");
+        System.out.println("This is my first Git push and pull practice.");
+    }
+}
+
